92ecc8739a42f18aedd69851590048b5ee4e1570
Tharunarjun <tharun.3c@gmail.com>
2016-07-05 18:44:53 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,19 +1,14 @@
-var arr= [];
-var arr1=[];
-console.log('fine6');
 function statusChangeCallback(response) {
     console.log('statusChangeCallback');
     console.log(response);
-    
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
-      
       testAPI();
-      
+      var arr= [];
       var count=0;
       FB.api('/me?fields=birthday', function(response1) {
    	console.log(response1.birthday);
@@ -66,15 +61,12 @@ date1=date1.toLocaleDateString();
           		}
         	}
         	console.log(arr);
-        	arr1=arr;
          });
 	y1=y1-1;
      	}
          
-     
      });
-    }
-    else if (response.status === 'not_authorized') {
+    } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       //document.getElementById('status').innerHTML = 'Please log into this app.';
       console.log("Please log into this app.");
@@ -87,8 +79,6 @@ date1=date1.toLocaleDateString();
         
     }
   }
- console.log('arr1 is '+arr1);
- console.log('arr is '+arr);
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
