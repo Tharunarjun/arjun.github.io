48970416979cae77cad74b830e7470294d9f3485
Tharunarjun <tharun.3c@gmail.com>
2016-07-10 19:54:16 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -39,7 +39,7 @@ date1=date1.toLocaleDateString();
      var ur='/me?fields=feed.since( '+ date + ').until(' + date1 + ')';
          FB.api(ur,function(response2){
          	//console.log(response2.feed.data[2]);
-        	for(j=0 ; j<response2.feed.data.length ; j++){
+        	for(j=0 ; j<=response2.feed.data.length ; j++){
         		console.log(response2.feed.data.length);
           	//	console.log(response2.feed.data[j]);
           		//console.log(response2.feed.data[j].story);
