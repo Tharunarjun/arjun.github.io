9b02db40d429d7f28c5d2afdd1a08ecf953a7f15
Tharunarjun <tharun.3c@gmail.com>
2016-07-10 16:54:31 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -14,7 +14,7 @@ function statusChangeCallback(response) {
       var count=0;
       FB.api('/me?fields=birthday', function(response1) {
    	console.log(response1.birthday);
-    var g=response1.birthday+2;
+    var g=response1.birthday+'2';
     console.log(g);
 var y1=2015;
 var m1=11;
