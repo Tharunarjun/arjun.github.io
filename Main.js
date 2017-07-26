9acab12f609585f92b5d7d89712d4e6680ca7f7b
Tharunarjun <tharun.3c@gmail.com>
2017-07-26 13:56:19 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,8 +1,6 @@
- 
-
 function statusChangeCallback(response) {
-    console.log('statusChangeCallback');
-    console.log(response);
+    //console.log('statusChangeCallback');
+    //console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
@@ -12,135 +10,158 @@ function statusChangeCallback(response) {
       testAPI();
       var arr= [];
       var arr1=[];
+      var arr2=[];
+	    var arr3=[];
       var count=0;
       var count1=0;
+     
       FB.api('/me?fields=birthday', function(response1) {
    	console.log(response1.birthday);
-    var b=new Date(response1.birthday);
+    
+var b=new Date(response1.birthday);
 	   	var b1=new Date(response1.birthday);
-	   	//var e=new Date();
+	   	
 	   	 var c=b1.getDate();
 	     b1.setDate(c + 2);
-//var y1=2015;
-//var m1=11;
-//var d1=08;
-//var m2=11;
-//var d2=10;
-var e = new Date();
+    var e = new Date();
+    var da=e.toLocaleDateString();
     var p = e.getFullYear();
     var y1=p;
-    console.log(y1);
-var y2=2015;
-//var y1=e.getFullYear();
+    var y2=2015;
+var count2=0;
 	var m1=b.getMonth();
 	var d1=b.getDate();
 	var m2=b1.getMonth();
 	var d2=b1.getDate();
-	//var y2=e.getFullYear();
-     	var k=y1;
+	var count3=y1-2009;
+	      //var count3=2011-2004;
+	      var count4=0;
      	var i=0;
      	var j=0;
-     	var f=0;
-//var url='/me?fields=feed.since(1449532800).until(1449705600)';
- for(k=y1; k>2011 ; k--){
-     	//	console.log(k);
-//(function(k){                                      //48,49
-  //  setTimeout(function(){
-
-	  var date = new Date(Date.UTC(k, m1, d1));
-	date=date.toLocaleDateString();
-	var date1 = new Date(Date.UTC(k, m2, d2));
-	date1=date1.toLocaleDateString();
-	//console.log(k);
-     //console.log(date);
-     //console.log(date1);
-     //setTimeout(function () {
-     		
-	 var ur='/me?fields=feed.since( '+ date + ').until(' + date1 + ')';
-       
+     	
+     	for(k=y1; k>2004 ; k--){
+     	
+   var c=new Date(Date.UTC(k,m1,d1)).getTime() / 1000;
+   console.log(c);
+   var c1=new Date(Date.UTC(k,m2,d2)).getTime() / 1000;
+   console.log(c1);
+   console.log("k is            "+k);
+     var ur='/me?fields=feed.since( '+ c + ').until(' + c1 + ')';
          FB.api(ur,function(response2){
-         	console.log(k);
-         	//console.log(response2.feed.data[2]);
+         console.log("k is    "+k);
+          /*if(!response2.feed){
+         	arr[count]=0;
+         	count++;
+         	arr1[count1]=k;
+         	console.log("k is "+ k);
+         	count1++;
+         }*/
+       if(response2.feed)
+         {
+         	
+         	
+         	console.log(response2.feed);
         	for(j=0 ; j<response2.feed.data.length ; j++){
-        		//console.log(response2.feed.data.length);
-          	//	console.log(response2.feed.data[j]);
+        	
           		console.log(response2.feed.data[j].story);
+          		var f=0;
+          	
           		if(response2.feed.data[j].story){
           		var str = response2.feed.data[j].story;
+          		
           		var str1=response2.feed.data[j].created_time;
           		var res1=str1.split("-");
-          		arr1[count1]=res1[0];
-          		count1++;
-          		console.log('yes'+response2.feed.data[j].story);
+          
+          		var b=str.includes("wrote");
+          		if(b)
+          		{
+          			
+          		 var x=false;	
     			var res = str.split(" ");
     			var v=res.length;
 			for(i=0;i<v;i++)
 			{
+			
+				
 				if(isNaN(res[i])==false)
 				{
 					var x=parseInt(res[i]);
 					x=x+1;
 					arr[count]=x;
 					count=count+1;
-				    //document.getElementById("demo").innerHTML = x;
-					console.log("yes "+x);
+					arr1[count1]=res1[0];
+						count1++;
+				  
 					f=1;
+				x=true;
 				}
-			}
+				
+				
+				
+				
 				
 			
-                	
-                	if(f==0)
+                	}
+          		
+               
+                	if(x==false)
+        		 
                 	{
                 		arr[count]=2;
                 		count++;
-                		console.log('Sir');
+                		f=-1;
+                		
+                			arr1[count1]=res1[0];
+						count1++;
+                	
                 	}
+          		 }
+          	
+          			
           		}
-          	/*	else{
-          			x++;
-          			arr[count]=x;
-					count=count+1;
-          			console.log(x);
-          		}*/
+          		
         	}
-        	console.log(arr);
-        	console.log(arr1);
-        	some(arr,arr1);
-         });
-     
-     	
-     //}, 3000)
-	//y1=y1-1;
-     	
          
-     
-    
- 
-      
-      
-// },  100)           //123,124
- //})(k);
- }
-      });
-    }
-    
-    else if (response.status === 'not_authorized') {
+         	
+         //
+         }
+        
+        	console.log(arr);
+		 if(arr.length==0)
+		 {
+			 count4++;
+			 console.log("count4 and count3 are  "+count4+" "+count3);
+			 
+		 }
+		 /*if(count4==count3)
+		 {
+			 alert("No data of posts present for you account ");
+		 }*/
+ // console.log("l is "+arr.length);
+		 
+		 check(count4,count3);
+        	some(arr,arr1);
+		 
+         });
+
+     	}
+         
+     });
+    } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
       //document.getElementById('status').innerHTML = 'Please log into this app.';
       console.log("Please log into this app.");
+	    alert("Please log into this app.");
         
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
       //document.getElementById('status').innerHTML = 'Please log into Facebook.';
       console.log("Please log into Facebook.");
+	    alert("Please log into Facebook.");
         
     }
-  
-    
-
-
+  }
   // This function is called when someone finishes with the Login
   // Button.  See the onlogin handler attached to it in the sample
   // code below.
@@ -149,6 +170,20 @@ var y2=2015;
       statusChangeCallback(response);
     });
   }
+   function myFunction() {
+    alert("You are logged in!!!  and Press the Click Here! button to see your Birthday posts");
+     //alert("Press the Click Here! button to see your Birthday posts");
+	    /*if(arr.length==0)
+		 {
+			 count4++;
+			 console.log("count4 and count3 are  "+count4+" "+count3);
+		 }
+		 if(count4==count3)
+		 {
+			 alert("No data of posts present for you account ");
+		 }*/
+}
+
   window.fbAsyncInit = function() {
   FB.init({
     appId      : '974830909219954',
@@ -192,5 +227,3 @@ var y2=2015;
        
     });
   }
-}  
-
